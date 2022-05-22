3b16d1718f5ed81c17a1b97cc7ab77175cf71919
abyusuft <imabuyusuf@gmail.com>
2022-05-23 00:35:50 +0600

    updated

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@doctors-portal.dzzpy.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const servicesCollection = client.db('doctor_portal').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('Doctors Server Running')
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening to Port : ${port}`)
+})
